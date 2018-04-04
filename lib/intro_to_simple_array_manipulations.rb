58f4235188d8b9095e1e27179daf12ae3a3b6c20
Eric Razor <eric.razorjr@gmail.com>
2018-04-04 03:20:25 +0000

    automatically backed up by learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,8 +27,7 @@ def shift_with_args(array)
 end
 
 def using_concat(arr_one,arr_two)
-  concatenated = arr_one+arr_two
-  arr_one + 1
+  concatenated = arr_one.push(arr_two)
   return concatenated
 end
   
